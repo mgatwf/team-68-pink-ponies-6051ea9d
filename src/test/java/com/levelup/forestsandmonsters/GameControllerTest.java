9457ed65e641af27c27a319d65455c70bd13af0a
Aneetha <naneetha@gmail.com>
2023-09-22 13:00:20 +0000

    added Testcases for GameController

--- src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
+++ src/test/java/com/levelup/forestsandmonsters/GameControllerTest.java
@@ -1,7 +1,11 @@
 package com.levelup.forestsandmonsters;
 
 import static org.junit.Assert.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.Test;
+
+import com.levelup.forestsandmonsters.GameController.DIRECTION;
 
 public class GameControllerTest {
     @Test
@@ -9,5 +13,29 @@ public class GameControllerTest {
         GameController testObj = new GameController();
         assertNotNull(testObj.status);
     }
+  @Test
+    public void moveCallsCharactersMove() {
+        GameController testObj = new GameController();
+        FakeCharacter mockCharacter = new FakeCharacter();
+        testObj.character = mockCharacter;
 
+        testObj.move(DIRECTION.EAST);
+
+        assertEquals(DIRECTION.EAST, mockCharacter.directionProvided);
+        assertEquals(mockCharacter.STUBBED_NEW_POSITION, testObj.character.currentPosition);
+        assertEquals(mockCharacter.STUBBED_NEW_POSITION.coordinates, testObj.getStatus().currentPosition);
+    }
+
+    @Test
+    public void startGameInitializesMapAndGivesToChracter() {
+        GameController testObj = new GameController();
+        FakeCharacter mockCharacter = new FakeCharacter();
+        testObj.character = mockCharacter;
+
+        testObj.startGame();
+
+        assertNotNull(testObj.map);
+        assertEquals(mockCharacter.STUBBED_NEW_POSITION, mockCharacter.currentPosition);
+
+    }
 }
